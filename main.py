369306db4a326f7637fdb078d8a232ef98574c9a
quere6 <malolasters095095@gmail.com>
2025-05-22 23:23:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from profiles.profile_handlers import start, help_cmd, profile_cmd, daily_cmd, reply_handler
+from shop.shop_handlers import shop_handler
+from duels.duels_handlers import duel_handler
+
+TOKEN = "7957837080:AAH1O_tEfW9xC9jfUt2hRXILG-Z579_w7ig"
+
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    # Основні команди
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_cmd))
+    app.add_handler(CommandHandler("profile", profile_cmd))
+    app.add_handler(CommandHandler("daily", daily_cmd))
+
+    # Інші хендлери
+    app.add_handler(shop_handler)
+    app.add_handler(duel_handler)
+
+    # Обробка текстових повідомлень
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply_handler))
+
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
