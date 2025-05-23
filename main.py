215b1d7b77864d5c531322f6f706199eacac3cce
quere6 <malolasters095095@gmail.com>
2025-05-23 13:27:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import asyncio
+from aiohttp import web
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from profiles.profile_handlers import start, help_cmd, profile_cmd, daily_cmd, reply_handler
 from shop.shop_handlers import shop_handler
 from duels.duels_handlers import duel_handler
+import os
 
 TOKEN = "7957837080:AAH1O_tEfW9xC9jfUt2hRXILG-Z579_w7ig"
+PORT = int(os.environ.get("PORT", 8000))  # Render задасть PORT у змінних оточення
+
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+async def run_web_server():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', PORT)
+    await site.start()
 
 async def main():
+    # Запускаємо одночасно і веб-сервер, і телеграм-бота
+    await run_web_server()
+
     app = ApplicationBuilder().token(TOKEN).build()
 
     # Основні команди
@@ -27,7 +44,4 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    try:
-        asyncio.get_event_loop().run_until_complete(main())
-    except RuntimeError:
-        asyncio.run(main())
+    asyncio.run(main())
